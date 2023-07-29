27512e79f9a5ff6dad3d9f21f83101c95bca9c1d
Dawid Kacza <dawidkacza02@gmail.com>
2023-07-29 18:22:14 +0200

    Implemented pagination logic

--- src/components/organisms/BookBrowser/BookBrowser.styles.js
+++ src/components/organisms/BookBrowser/BookBrowser.styles.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-grow: 1;
+  display: grid;
+  grid-template-columns: 65% 1fr;
+  column-gap: 1rem;
+  grid-template-rows: 85% 15%;
+  height: 100%;
   
-  .table-wrapper {
-    flex-basis: 66%;
-  }
+
 `;
 
 export default Wrapper;
