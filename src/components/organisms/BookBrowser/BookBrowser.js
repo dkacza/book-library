4a494816e0382f3c2a559857ed4ac7ce4bf8be58
Dawid Kacza <dawidkacza02@gmail.com>
2023-07-30 15:06:56 +0200

    Integrated react use form hook

--- src/components/organisms/BookBrowser/BookBrowser.js
+++ src/components/organisms/BookBrowser/BookBrowser.js
@@ -1,117 +1,34 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from 'components/organisms/Table/Table';
 import Filters from 'components/organisms/Filters/Filters';
 import Wrapper from 'components/organisms/BookBrowser/BookBrowser.styles';
 import axios from 'api/axios';
 import Pagination from 'components/molecules/Pagination/Pagination';
+import { useForm } from 'react-hook-form';
 
 const columnNames = ['Title', 'Authors', 'ISBN', 'Status'];
 const columnCodes = ['title', 'authors', 'isbn', 'status'];
 const LIMIT_PER_PAGE = 10;
 const INITIAL_PAGE = 1;
-const INITIAL_FILTER_STATE = {
-  searchQuery: '',
-  releaseYear: {
-    from: null,
-    to: null,
-  },
-  genre: {
-    fiction: true,
-    nonFiction: true,
-    scientific: true,
-    children: true,
-    poetry: true,
-  },
-  availableOnly: false,
-};
-const ACTION_TYPES = {
-  toggleFiction: 'TOGGLE_FICTION',
-  toggleNonFiction: 'TOGGLE_NON_FICTION',
-  toggleScientific: 'TOGGLE_SCIENTIFIC',
-  toggleChildren: 'TOGGLE_CHILDREN',
-  togglePoetry: 'TOGGLE_POETRY',
-  toggleAvailability: 'TOGGLE_AVAILABILITY',
-  changeFromDate: 'CHANGE_FROM_DATE',
-  changeToDate: 'CHANGE_TO_DATE',
-  changeSearchQuery: 'CHANGE_SEARCH_QUERY'
-};
-const reducer = (state, action) => {
-  switch (action.type) {
-    case ACTION_TYPES.toggleFiction:
-      return {
-        ...state,
-        genre: {
-          ...state.genre,
-          fiction: !state.genre.fiction,
-        },
-      };
-    case ACTION_TYPES.toggleNonFiction:
-      return {
-        ...state,
-        genre: {
-          ...state.genre,
-          nonFiction: !state.genre.nonFiction,
-        },
-      };
-    case ACTION_TYPES.toggleScientific:
-      return {
-        ...state,
-        genre: {
-          ...state.genre,
-          scientific: !state.genre.scientific,
-        },
-      };
-    case ACTION_TYPES.toggleChildren:
-      return {
-        ...state,
-        genre: {
-          ...state.genre,
-          children: !state.genre.children,
-        },
-      };
-    case ACTION_TYPES.togglePoetry:
-      return {
-        ...state,
-        genre: {
-          ...state.genre,
-          poetry: !state.genre.poetry,
-        },
-      };
-    case ACTION_TYPES.toggleAvailability:
-      return {
-        ...state,
-        availableOnly: !state.availableOnly,
-      };
-    case ACTION_TYPES.changeFromDate:
-      return {
-        ...state,
-        releaseYear: {
-          ...state.releaseYear,
-          from: action.newValue,
-        },
-      };
-    case ACTION_TYPES.changeToDate:
-      return {
-        ...state,
-        releaseYear: {
-          ...state.releaseYear,
-          to: action.newValue,
-        },
-      };
-    case ACTION_TYPES.changeSearchQuery:
-      return {
-        ...state,
-        searchQuery: action.newValue
-      };
-    default:
-      return state;
-  }
-};
 
 const BookBrowser = () => {
   const [books, setBooks] = useState([]);
   const [pages, setPages] = useState({});
-  const [filters, dispatch] = useReducer(reducer, INITIAL_FILTER_STATE);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      fiction: true,
+      nonFiction: true,
+      scientific: true,
+      children: true,
+      poetry: true,
+      yearFrom: 1800,
+      yearTo: new Date().getFullYear(),
+    },
+  });
   const getBookData = (page, limit) => {
     axios
       .get(`/books?page=${page}&limit=${limit}`)
@@ -144,15 +61,23 @@ const BookBrowser = () => {
     }
     getBookData(newPage, LIMIT_PER_PAGE);
   };
-  const onSubmitFilters = (e) => {
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log('Filters submitted');
+  };
+  const onError = (err) => {
+    console.log('Filters submitted with error');
+  };
+
+  const submitWithPrevent = (e) => {
     e.preventDefault();
-    console.log(filters);
-  }
+    handleSubmit(onSubmit, onError)();
+  };
 
   return (
     <Wrapper>
       <Table columnNames={columnNames} columnCodes={columnCodes} data={books} />
-      <Filters filters={filters} dispatch={dispatch} ACTION_TYPES={ACTION_TYPES} onSubmit={onSubmitFilters}/>
+      <Filters onSubmit={(e) => submitWithPrevent(e)} register={register}/>
       <Pagination pages={pages} handlePageChange={handlePageChange}></Pagination>
     </Wrapper>
   );
--- src/components/organisms/Filters/Filters.js
+++ src/components/organisms/Filters/Filters.js
@@ -5,95 +5,27 @@ import { ReactComponent as MyIcon } from 'assets/icons/search_FILL0_wght600_GRAD
 import LabeledCheckbox from 'components/molecules/LabeledCheckbox';
 import SubmitButton from 'components/atoms/SubmitButton';
 
-const Filters = ({ filters, dispatch, ACTION_TYPES, onSubmit }) => {
-  const handleInputChange = (e, type) => {
-    dispatch({
-      type,
-      newValue: e.target.value,
-    });
-  };
-
+const Filters = ({ register, onSubmit, formState }) => {
   return (
     <form className="filter-wrapper" onSubmit={onSubmit}>
-      <InputWithIcon
-        name="search"
-        id="search"
-        type="text"
-        Icon={MyIcon}
-        value={filters.searchQuery}
-        onChange={(e) => handleInputChange(e, ACTION_TYPES.changeSearchQuery)}
-      />
+      <InputWithIcon name="search" id="search" type="text" Icon={MyIcon} {...register('searchQuery')}/>
       <div className="release-year-filter">
         <p>Release year:</p>
-        <input
-          type="text"
-          id="date-from-input"
-          placeholder="from"
-          value={filters.releaseYear.from || ''}
-          onChange={(e) => handleInputChange(e, ACTION_TYPES.changeFromDate)}
-        />
+        <input type="number" id="date-from-input" placeholder="from" {...register('yearFrom', {validate: (val, formValues) => val <= formValues.yearTo || 'Ending date must be greater or equal to starting date'})} />
         <p>-</p>
-        <input
-          type="text"
-          id="date-to-input"
-          placeholder="to"
-          value={filters.releaseYear.to || ''}
-          onChange={(e) => handleInputChange(e, ACTION_TYPES.changeToDate)}
-        />
+        <input type="number" id="date-to-input" placeholder="to" {...register('yearTo')} />
       </div>
       <div className="genre-filter">
         <p>Genre:</p>
-        <LabeledCheckbox
-          id="fiction"
-          label="Fiction literature"
-          name="fiction"
-          value={filters.genre.fiction}
-          checked={filters.genre.fiction}
-          onChange={() => dispatch({ type: ACTION_TYPES.toggleFiction })}
-        />
-        <LabeledCheckbox
-          id="non-fiction"
-          label="Non-fiction literature"
-          name="non-fiction"
-          value={filters.genre.nonFiction}
-          checked={filters.genre.nonFiction}
-          onChange={() => dispatch({ type: ACTION_TYPES.toggleNonFiction })}
-        />
-        <LabeledCheckbox
-          id="science"
-          label="Scientific literature"
-          name="scientific"
-          value={filters.genre.scientific}
-          checked={filters.genre.scientific}
-          onChange={() => dispatch({ type: ACTION_TYPES.toggleScientific })}
-        />
-        <LabeledCheckbox
-          id="children"
-          label="Children literature"
-          name="children"
-          value={filters.genre.children}
-          checked={filters.genre.children}
-          onChange={() => dispatch({ type: ACTION_TYPES.toggleChildren })}
-        />
-        <LabeledCheckbox
-          id="poetry"
-          label="Poetry and drama"
-          name="poetry"
-          value={filters.genre.poetry}
-          checked={filters.genre.poetry}
-          onChange={() => dispatch({ type: ACTION_TYPES.togglePoetry })}
-        />
+        <LabeledCheckbox id="fiction" label="Fiction literature" name="fiction" {...register('fiction')} />
+        <LabeledCheckbox id="non-fiction" label="Non-fiction literature" name="non-fiction" {...register('nonFiction')}  />
+        <LabeledCheckbox id="science" label="Scientific literature" name="scientific" {...register('scientific')} />
+        <LabeledCheckbox id="children" label="Children literature" name="children" {...register('children')} />
+        <LabeledCheckbox id="poetry" label="Poetry and drama" name="poetry" {...register('poetry')} />
       </div>
       <div className="availability-filter">
         <p>Availability:</p>
-        <LabeledCheckbox
-          id="available"
-          label="Display only available books"
-          name="available"
-          value={filters.availableOnly}
-          checked={filters.availableOnly}
-          onChange={() => dispatch({ type: ACTION_TYPES.toggleAvailability })}
-        />
+        <LabeledCheckbox id="available" label="Display only available books" name="available" {...register('availableOnly')}/>
       </div>
       <SubmitButton type="submit">Apply filters</SubmitButton>
     </form>
