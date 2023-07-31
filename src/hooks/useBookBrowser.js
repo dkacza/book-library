92a77c6c6c0079d44e8e655cc83da6b18a131709
Dawid Kacza <dawidkacza02@gmail.com>
2023-07-31 12:14:20 +0200

    Implemented useBookBrowser hook responsible for state in book view

---
+++ src/hooks/useBookBrowser.js
@@ -0,0 +1,106 @@
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'api/axios';
+
+const useBookBrowser = (initialFormValues, limitPerPage, initialPage) => {
+  const [books, setBooks] = useState([]);
+  const [pages, setPages] = useState({});
+  const [query, setQuery] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ defaultValues: initialFormValues });
+
+  const getBookData = (page, limit) => {
+    axios
+      .get(`/books?page=${page}&limit=${limit}&${query}`)
+      .then((res) => {
+        const booksResponse = res.data.data.books;
+        const preparedBooks = booksResponse.map((book) => {
+          const newBookObj = {
+            ...book,
+            authors: book.authors.map((author) => author.name).join('\n'),
+            status: book.currentStatus,
+          };
+          delete newBookObj.currentStatus;
+          return newBookObj;
+        });
+        setBooks(preparedBooks);
+
+        const paginationResponse = res.data.data.pagination;
+        setPages(paginationResponse);
+      })
+      .catch((err) => console.log(`Could not fetch the data about books`));
+  };
+
+  const onSubmit = (data) => {
+    setQuery(buildQuery(data));
+    getBookData(initialPage, limitPerPage);
+  };
+  const onError = (err) => {
+    reset();
+  };
+
+  const buildQuery = (data) => {
+    let queryString = '';
+
+    // Set genre filter
+    const { genre } = data;
+    let genreString = 'genre=';
+    let emptyGenre = true;
+    for (const [key, value] of Object.entries(genre)) {
+      if (!value) continue;
+      genreString += key + ',';
+      emptyGenre = false;
+    }
+    if (!emptyGenre) {
+      genreString = genreString.substring(0, genreString.length - 1);
+      queryString += genreString + '&';
+    }
+
+    // Set available only filter
+    if (data.availableOnly) {
+      queryString += 'availableOnly=true&';
+    }
+
+    // Set year filter
+    const startDate = new Date();
+    startDate.setFullYear(data.yearFrom);
+    queryString += 'publicationDate[gte]=' + startDate.getFullYear() + '&';
+
+    const endDate = new Date();
+    endDate.setFullYear(data.yearTo);
+    queryString += 'publicationDate[lte]=' + endDate.getFullYear();
+
+    // TODO Add search query
+    console.log(queryString);
+
+    return queryString;
+  };
+
+  const submitWithPrevent = (e) => {
+    e.preventDefault();
+    handleSubmit(onSubmit, onError)();
+  };
+  const handlePageChange = (newPage) => {
+    if (newPage > pages.totalPages || newPage < 1) {
+      return;
+    }
+    getBookData(newPage, limitPerPage);
+  };
+
+  useEffect(() => {
+    getBookData(initialPage, limitPerPage);
+  }, []);
+
+  return {
+    pages,
+    books,
+    register,
+    handlePageChange,
+    submitWithPrevent
+  }
+}
+export default useBookBrowser;
