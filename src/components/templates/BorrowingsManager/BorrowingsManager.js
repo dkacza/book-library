eb109b53059aaa4e7c01278cf94480cc5345f56a
Dawid Kacza <dawidkacza02@gmail.com>
2023-08-01 19:41:43 +0200

    Implemented browsing borrowed books

--- src/components/templates/BorrowingsManager/BorrowingsManager.js
+++ src/components/templates/BorrowingsManager/BorrowingsManager.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
-import StyledSection from 'components/templates/BorrowingsManager/BorrowingsManager.styles';
+import StyledBorrowingsManager from 'components/templates/BorrowingsManager/BorrowingsManager.styles';
 import UserSelection from 'components/organisms/UserSelection/UserSelection';
+import BookBorrowing from 'components/organisms/BookBorrowing/BookBorrowing';
 
 const BorrowingsManager = () => {
   const [selectedUser, setSelectedUser] = useState();
+  const handleUserDelete = (e) => {
+    e.preventDefault();
+    setSelectedUser(undefined);
+  }
 
   return (
-    <StyledSection className="wrapper">
-      {!selectedUser ? <UserSelection setSelectedUser={setSelectedUser} /> : <div>USER SELECTED</div>}
-    </StyledSection>
+    <StyledBorrowingsManager className="borrowings-manager">
+      {selectedUser ? (
+        <BookBorrowing selectedUser={selectedUser} handleUserDelete={handleUserDelete} />
+      ) : (
+        <UserSelection setSelectedUser={setSelectedUser} />
+      )}
+    </StyledBorrowingsManager>
   );
 };
 
