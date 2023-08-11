bf6ee2a879e175adeeeda42cdf1ddf7cd0c3c873
Dawid Kacza <dawidkacza02@gmail.com>
2023-08-11 13:06:25 +0200

    Implemented basic validation and error message displaying

--- src/utils/validationRegexes.js
+++ src/utils/validationRegexes.js
@@ -3,6 +3,7 @@ const validationRegexes = {
   emailRegex: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
   phoneRegex: /^(?:(?:\+?\d{1,3}\s?)?(?:\(\d{1,}\)|\d{1,})[-.\s]?){1,}\d{1,}$/,
   passwordRegex: /^(?=.*[A-Za-zĄĆĘŁŃÓŚŹŻąćęłńóśźż])(?=.*\d).{8,}$/,
+  authorsNamesRegex: /^([A-Za-zęóąśłżźćńĘÓĄŚŁŻŹĆŃ]+\s+[A-Za-zęóąśłżźćńĘÓĄŚŁŻŹĆŃ]+)(,\s*[A-Za-zęóąśłżźćńĘÓĄŚŁŻŹĆŃ]+\s+[A-Za-zęóąśłżźćńĘÓĄŚŁŻŹĆŃ]+)*$/,
 };
 
 export default validationRegexes;
--- src/views/MainViews/AddBookView/AddBookView.js
+++ src/views/MainViews/AddBookView/AddBookView.js
@@ -5,52 +5,67 @@ import SimpleInput from 'components/atoms/SimpleInput';
 import TextArea from 'components/atoms/TextArea';
 import SubmitButton from 'components/atoms/SubmitButton';
 import { useForm } from 'react-hook-form';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'api/axios';
-
-
+import validationRegexes from 'utils/validationRegexes';
 
 const AddBookView = () => {
-  const { register, errors, handleSubmit, getValues } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    getValues,
+  } = useForm();
   const [file, setFile] = useState();
+  const [errorMsg, setErrorMsg] = useState('');
+  const [successMsg, setSuccessMsg] = useState('');
 
   const postBook = (data) => {
     // process authors
-    const newAuthors = data.authors.split(',').map(author => ({name: author}));
-    console.log(newAuthors)
+    const newAuthors = data.authors.split(',').map((author) => ({ name: author }));
+    console.log(newAuthors);
     const requestBody = {
       ...data,
-      authors: newAuthors
-    }
+      authors: newAuthors,
+    };
     if (file) requestBody.bookCoverPhoto = file;
 
     console.log(requestBody);
-    axios.post(`books`, requestBody, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      }
-    }).then(res => {
-      console.log(res);
-    }).catch(err => {
-      console.log(err);
-    })
-  }
+    axios
+      .post(`books`, requestBody, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((res) => {
+        setSuccessMsg('Book has been created')
+      })
+      .catch((err) => {
+        setErrorMsg(err.response.data.message || 'Something went wrong');
+      });
+  };
 
   const onSubmit = (data) => {
     postBook(data);
-  }
+  };
   const onError = (err) => {
-    console.log(err);
-  }
+    setErrorMsg('Validation rules violated');
+  };
   const submitWithPrevent = (e) => {
-    console.log('submission')
     e.preventDefault();
+    setSuccessMsg('');
+    setErrorMsg('');
     handleSubmit(onSubmit, onError)();
-  }
+  };
   const handleImageSelection = (e) => {
     setFile(e.target.files[0]);
-    console.log(e.target.files[0])
-  }
+    console.log(e.target.files[0]);
+  };
+
+  useEffect(() => {
+    setSuccessMsg('');
+    setErrorMsg('');
+  }, []);
 
   return (
     <MainViewTemplate>
@@ -60,40 +75,63 @@ const AddBookView = () => {
         <form onSubmit={(e) => submitWithPrevent(e)}>
           <div className="title">
             <p className="label">Title</p>
-            <SimpleInput {...register('title')} />
+            <SimpleInput {...register('title', { required: true })} className={errors.title ? 'error' : ''} />
           </div>
           <div className="authors">
             <p className="label">Authors</p>
             <p className="additional-info">Separate different authors with "," sign</p>
-            <SimpleInput {...register('authors')}/>
+            <SimpleInput
+              {...register('authors', {
+                required: true,
+                validate: (val) => validationRegexes.authorsNamesRegex.test(val),
+              })}
+              className={errors.authors ? 'error' : ''}
+            />
           </div>
           <div className="publication-date">
             <p className="label">Publication year</p>
-            <SimpleInput type="date" {...register('publicationDate')}/>
+            <SimpleInput
+              type="date"
+              {...register('publicationDate', { required: true })}
+              className={errors.publicationDate ? 'error' : ''}
+            />
           </div>
           <div className="isbn">
             <p className="label">ISBN</p>
-            <SimpleInput type="number" {...register('isbn')} />
+            <SimpleInput
+              type="number"
+              {...register('isbn', { required: true })}
+              className={errors.isbn ? 'error' : ''}
+            />
           </div>
-          <div className='genre'>
-            <p className='label'>Genre</p>
-            <select name='genre' id='genre' {...register('genre')}>
-              <option value='fiction'>Fiction</option>
-              <option value='nonFiction'>Non-fiction</option>
-              <option value='poetry'>Poetry</option>
-              <option value='scientific'>Scientific</option>
-              <option value='children'>Children literature</option>
+          <div className="genre">
+            <p className="label">Genre</p>
+            <select
+              name="genre"
+              id="genre"
+              {...register('genre', { required: true })}
+              className={errors.genre ? 'error' : ''}
+            >
+              <option value="fiction">Fiction</option>
+              <option value="nonFiction">Non-fiction</option>
+              <option value="poetry">Poetry</option>
+              <option value="scientific">Scientific</option>
+              <option value="children">Children literature</option>
             </select>
           </div>
-          <div className="description" >
+          <div className="description">
             <p className="label">Description</p>
-            <TextArea {...register('description')}/>
+            <TextArea {...register('description')} />
           </div>
           <div className="photo-input">
             <p className="label">Cover image</p>
-            <input type="file" onChange={(e) => handleImageSelection(e)}/>
+            <input type="file" onChange={(e) => handleImageSelection(e)} />
             <p className="selected-info">{file ? file.name : 'Default cover photo will be applied'}</p>
           </div>
+
+          {errorMsg ? <p className='error-msg'>{errorMsg}</p> : ''}
+          {successMsg ? <p className='success-msg'>{successMsg}</p> : ''}
+
           <SubmitButton>Add book</SubmitButton>
         </form>
       </main>
