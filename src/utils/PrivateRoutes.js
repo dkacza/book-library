b67c5dac4d437eb657ad4a28df9c305f8d71b2a8
Dawid Kacza <dawidkacza02@gmail.com>
2023-08-28 09:56:35 +0200

    Fixed redirection messages

--- src/utils/PrivateRoutes.js
+++ src/utils/PrivateRoutes.js
@@ -1,17 +1,21 @@
 import { useContext, useEffect } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import AuthContext from 'providers/AuthProvider';
+import isEmptyObject from 'utils/isEmptyObject';
 
 
 const PrivateRoutes = ({ permittedRoles }) => {
   const { auth, setRedirectionError } = useContext(AuthContext);
   const permission = permittedRoles.includes(auth.role);
+
+
   useEffect(() => {
-    const unpermittedAccessMessage = permission ? '' :
+    const unpermittedAccessMessage = !permission && !isEmptyObject(auth) ? '' :
       `You are not permitted to access this route.
     Log into authorized account to continue.`
     setRedirectionError(unpermittedAccessMessage);
-  }, [])
+  }, [auth])
+
   return permission ? <Outlet /> : <Navigate to="/login" />;
 };
 export default PrivateRoutes;
